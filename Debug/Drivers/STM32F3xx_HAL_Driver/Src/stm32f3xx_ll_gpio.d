e4832323837b54b671581a1781397b76b6ce043b
xmartonm <36737275+xmartonm@users.noreply.github.com>
2020-10-27 10:11:35 +0100

    oprava

--- Debug/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_gpio.d
+++ Debug/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_gpio.d
@@ -4,9 +4,10 @@ Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_gpio.o: \
  ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
  ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303x8.h \
  ../Drivers/CMSIS/Include/core_cm4.h \
- ../Drivers/CMSIS/Include/cmsis_version.h \
- ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/core_cmInstr.h \
  ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/core_cmFunc.h \
+ ../Drivers/CMSIS/Include/core_cmSimd.h \
  ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
  ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_ll_bus.h
 
@@ -18,11 +19,13 @@ Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_gpio.o: \
 
 ../Drivers/CMSIS/Include/core_cm4.h:
 
-../Drivers/CMSIS/Include/cmsis_version.h:
-
-../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/core_cmInstr.h:
 
 ../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/core_cmFunc.h:
+
+../Drivers/CMSIS/Include/core_cmSimd.h:
 
 ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
 
--- Debug/sources.mk
+++ Debug/sources.mk
@@ -19,7 +19,7 @@ OBJCOPY_BIN :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-Core/Src \
-Core/Startup \
 Drivers/STM32F3xx_HAL_Driver/Src \
+Src \
+Startup \
 
